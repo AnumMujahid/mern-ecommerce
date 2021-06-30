cc63c3de5aa70b33617c102e09d8efa3d5413090
Anum Mujahid <amujahid.bese18seecs@student.nust.edu.pk>
2021-06-30 10:43:32 +0500

    feat(): add header, footer and homescreen component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
 const App = () => {
   return (
-    <div>
-    </div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
