8d7acd7d7db40947e4257f9e4371f853efcf2855
Anum Mujahid <amujahid.bese18seecs@student.nust.edu.pk>
2021-07-05 21:26:51 +0500

    feat(): add user authentication

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,11 +4,13 @@ import {
   authUser,
   registerUser,
   getUserProfile,
+  updateUserProfile,
 } from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 router.post('/login', authUser);
 router.post('/register', registerUser);
 router.get('/profile', protect, getUserProfile);
+router.put('/profile', protect, updateUserProfile);
 
 export default router;
---
+++ frontend/src/screens/ShippingScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShippingScreen = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ShippingScreen
